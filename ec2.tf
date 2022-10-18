e31c8b45b367d9db22f35c0d87febdc6d3c4aaa2
mahendarwa <48686711+mahendarwa@users.noreply.github.com>
2022-10-18 23:24:47 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,12 @@
 provider "aws" {
   profile = "default"
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIAWGWBMB3AOZPGRPQK"
+  secret_key = "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"
   region  = "ap-southeast-1"
 }
 
 resource "aws_instance" "app_server" {
   ami           = "ami-082105f875acab993"
   instance_type = "t2.micro"
-
-  
-
 }
 
